b5638acdede7db8af495f8d4fb4fb42ea107dd4c
lilian Robert <mslillianito@gmail.com>
2021-05-28 22:46:06 -0700

    added validations to the forms post data

--- validated_show_app/models.py
+++ validated_show_app/models.py
@@ -1,11 +1,26 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class ShowManager(models.Manager):
+    def validate(self, fieldData):
+        errors = {}
+        if len(fieldData['title']) < 2:
+            errors['title'] = 'Title field should be at least 2 characters'
+        if len(fieldData['network']) < 2:
+            errors['network'] = 'Network field should be at least 2 characters'
+        if fieldData['description'] != '' and len(fieldData['description']) < 8:
+            errors['description'] = 'Description should be at least 8 characters'
+        if datetime.strptime(fieldData['release_date'], '%Y-%m-%d') > datetime.now():
+            errors['release_date'] = 'Release Date should be in the past'
+        return errors
 class Show(models.Model):
     title= models.CharField(max_length=255)
     network= models.CharField(max_length=255)
-    release_date= models.DateField(auto_now_add=False, auto_now=False, null=True)
-    description= models.TextField()
+    release_date= models.DateField(auto_now_add=False, auto_now=False, blank=True)
+    description= models.TextField(blank=True)
     created_at= models.DateTimeField(auto_now_add=True)
     updated_at= models.DateTimeField(auto_now=True)
+
+    objects=ShowManager()
 
--- validated_show_app/views.py
+++ validated_show_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Show
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -14,15 +15,21 @@ def new_show(request):
 def back(request):
     return redirect("/shows")
 def create(request):
-    if request.method== "POST":
+    errors = Show.objects.validate(request.POST)
+    if len(errors) >0:
+        for (key, value) in errors.items():
+            messages.error(request, value)
+        return redirect('/new')
+    else:
+        if 'release_date' not in request.POST:
+            request.POST.get['release_date'] = '' 
         Show.objects.create(
-        title=request.POST['title'],
-        network=request.POST['network'],
-        release_date= request.POST['release_date'],
-        description=request.POST['description'],
+            title=request.POST['title'],
+            network=request.POST['network'],
+            release_date= request.POST['release_date'],
+            description=request.POST['description'],
         )
-    if 'release_date' not in request.POST:
-        request.POST.get['release_date'] = '' or None  
+    
     return redirect('/shows')
 def show(request, show_id):
     show_select = Show.objects.get(id=show_id)
@@ -37,13 +44,20 @@ def edit(request, show_id):
     }
     return render(request, 'edit.html', context)
 def update(request, show_id):
-    show_update = Show.objects.get(id=show_id)
-    show_update.title = request.POST['title']
-    show_update.release_date = request.POST['release_date']
-    show_update.network = request.POST['network']
-    show_update.description = request.POST['description']
-    show_update.save()
-    return redirect(f'/edit/{ show_id }')
+    errors = Show.objects.validate(request.POST)
+    if len(errors) >0:
+        for (key, value) in errors.items():
+            messages.error(request, value)
+        return redirect(f'/edit/{ show_id }')
+    else:
+        show_update = Show.objects.get(id=show_id)
+        show_update.title = request.POST['title']
+        show_update.release_date = request.POST['release_date']
+        show_update.network = request.POST['network']
+        show_update.description = request.POST['description']
+        show_update.save()
+        messages.success(request, "Show successfully updated")
+        return redirect(f'/show/{ show_id }')
 def delete(request, show_id):
     d = Show.objects.get(id=show_id)
     d.delete()
